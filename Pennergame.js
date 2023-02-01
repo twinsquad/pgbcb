5ff6fec548e44ed67543c1d465b8b506cba8a450
twinsquad <41787836+twinsquad@users.noreply.github.com>
2023-02-01 17:15:22 +0100

    Add files via upload

---
+++ Pennergame.js
@@ -0,0 +1,208 @@
+var events = require('events'),
+    request = require('request'),
+    DOM = require('jsdom');
+
+/**
+ * Pennergame Class constructor
+ *
+ * This will create a Pennergame instance and log you into
+ * pennergame.de. It'll emit an 'loggedin' event on success.
+ *
+ * user [Object] { username: 'yourusernamehere', password: 'yourpasswordherer' }
+ * city [String] One of Pennergame.Cities, defaults to Berlin
+ */
+function Pennergame(user, city) {
+    var req, document, self = this;
+
+    user = user;
+    city = city || 'Berlin';
+
+    req = request.defaults({
+        baseUrl: Pennergame.Cities[city],
+        followAllRedirects: true,
+        jar: true // Save cookies (in Memory)
+    });
+
+    /**
+     * Send a POST request with "x-www-form-urlencoded" Content-Type to path
+     *
+     * Emits an error event if there's an error.
+     *
+     * path [String] The path to send the request to.
+     * data [Object] The Form Data to send with the request.
+     * callback [Function]
+     */
+    this.postIt = function (path, data, callback) {
+        req.post({ url: path, form: data }, function (error, response, body) {
+            if (error || response.statusCode >= 400) {
+                self.emit('error', error || 'HttpError: ' + response.statusCode);
+            } else {
+                DOM.env(body, function (errors, window) {
+                    if (errors) {
+                        self.emit('error', errors);
+                    } else {
+                        document = window.document;
+                    }
+                    callback(getState());
+                });
+            }
+        });
+    };
+
+    /**
+     * Find error elements and extract their message.
+     *
+     * return [Array] An array of error messages.
+     */
+    this.getErrors = function () {
+        var error_elements = document.getElementsByClassName('errmsg'),
+            errors = [];
+
+        if (error_elements) {
+            // We'll need an Array to .map() over it.
+            error_elements = Array.prototype.slice.call(error_elements);
+            errors = error_elements.map(function (error_element) {
+                return error_element.textContent.trim();
+            });
+        }
+        return errors;
+    };
+
+    /**
+     * Get remaining time for a collect.
+     *
+     * If there is a running collect, there will be a progressbar
+     * which contains a bit of Javascript that passes the remaining
+     * time of the collect, in seconds, to a function called `counter`
+     *
+     *     counter(570);
+     *
+     * return [Number] Remaining time of current collect.
+     */
+    this.getRemainingTime = function () {
+        var remaining_time = -1,
+            processbar = document.getElementsByClassName('processbar_bg')[0],
+            counter;
+
+        if (processbar) {
+            if (counter = processbar.getElementsByTagName('script')[0]) {
+                remaining_time = counter.textContent.match(/counter\((\d+)\)/)[1];
+            }
+        }
+
+        return remaining_time;
+    };
+
+    /**
+     * Search the document for various Elements
+     * to define in which state the Game currently is.
+     *
+     * return [Number] One of Pennergame.States
+     */
+    function getState() {
+        var state = Pennergame.States.LOGGEDOUT,
+            notification, button;
+
+        if (!document) {
+            return Pennergame.States.UNKNOWN;
+        }
+
+        if (document.getElementById('my-profile-new')) {
+            state = Pennergame.States.LOGGEDIN;
+
+            button = document.getElementsByClassName('button_skill')[0];
+            notification = document.getElementById('ntext');
+
+            if (notification && notification.textContent) {
+                if (notification.textContent.match(/gedulde/)) {
+                    state = Pennergame.States.PENDING_COLLECT;
+
+                    if (button && button.value === 'Einkaufswagen ausleeren') {
+                        state = Pennergame.States.PENDING_CART;
+                    }
+                } else {
+                    state = Pennergame.States.COLLECTING;
+                }
+            } else {
+                state = Pennergame.States.PENDING_CART;
+            }
+        }
+
+        return state;
+    }
+
+    // Login
+    self.postIt('login/check/', user, function (state) {
+        if (state <= Pennergame.States.LOGGEDOUT) {
+            self.emit('error', self.getErrors());
+        } else {
+            self.emit('loggedin');
+        }
+    });
+};
+
+// The various stats our game can have.
+Pennergame.States = Object.freeze({
+    UNKNOWN: 0,
+    LOGGEDOUT: 1,
+    LOGGEDIN: 2,
+    COLLECTING: 3,
+    PENDING_COLLECT: 4,
+    PENDING_CART: 5
+});
+
+// Availabile Cities and their URLs.
+Pennergame.Cities = Object.freeze({
+    'Vatikan': 'http://vatikan.pennergame.de',
+    'Sylt': 'http://sylt.pennergame.de',
+    'Malle': 'http://malle.pennergame.de',
+    'Hamburg': 'http://www.pennergame.de',
+    'Hamburg Reloaded': 'http://reloaded.pennergame.de',
+    'Köln': 'http://koeln.pennergame.de',
+    'Berlin': 'https://berlin.pennergame.de',
+    'München': 'http://muenchen.pennergame.de'
+});
+
+/**
+ * Starts collecting Bottles for `minutes` Minutes
+ */
+Pennergame.prototype.collect = function (minutes) {
+    var self = this;
+    minutes = minutes || 10;
+    self.postIt('activities/bottle/', 
+				{ sammeln: minutes, konzentrieren: 1 },
+                function (state) {
+                    switch(state) {
+						case Pennergame.States.COLLECTING:
+							self.emit('start_collect', self.getRemainingTime());
+							break;
+
+						case Pennergame.States.PENDING_COLLECT:
+							self.emit('pending_collect', self.getRemainingTime());
+							break;
+
+						case Pennergame.States.PENDING_CART:
+							self.emit('pending_cart');
+							break;
+
+						default:
+							self.emit('error', 'Unexpected state: ' + state);
+							break;
+                    }
+                });
+};
+
+/**
+ * Clear out your cart after a collect.
+ */
+Pennergame.prototype.clear_cart = function () {
+    var self = this;
+    self.postIt('activities/bottle/', { bottlecollect_pending: true }, function (state) {
+        self.emit('clear_cart', state);
+    });
+};
+
+// Inherit from events.EventEmitter.
+Pennergame.prototype.__proto__ = events.EventEmitter.prototype;
+
+module.exports = Pennergame;
---
+++ pgbcb.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+//#!C:\Program Files\nodejs\node.exe
+
+var program = require('commander'),
+    Pennergame = require('./Pennergame'),
+    formatTime = require('./utils').formatTime,
+    game;
+
+program
+    .option('-u, --username <username>', 'Your username')
+    .option('-p, --password <password>', 'Your password')
+    .option('-c, --city <city>', 'The City to log in. Default: Berlin')
+    .option('-t, --time <minutes>', 'How long each collection should last.' +
+            ' Default: 10', parseInt, 10);
+
+program.parse(process.argv);
+
+if (process.argv.slice(2).length === 0) {
+    program.help();
+}
+
+if (!program.username) {
+    console.error('Username is required');
+    process.exit(1);
+}
+
+if (!program.password) {
+    console.error('Password is required');
+    process.exit(1);
+}
+
+
+var valid_cities = Object.keys(Pennergame.Cities);
+if (program.city && valid_cities.indexOf(program.city) === -1) {
+
+    console.error('Invalid City:', program.city);
+    console.error('Valid choices are:');
+    valid_cities.forEach(function (city, index) {
+        console.error('\t%d. %s', index + 1, city);
+    });
+
+    process.exit(1);
+}
+
+console.log('Trying to log in %s as: %s',
+            program.city || 'Berlin',
+            program.username);
+
+game = new Pennergame({
+    username: program.username,
+    password: program.password
+}, program.city);
+
+game.on('error', function (errors) {
+    console.error('Error:', errors);
+});
+
+game.on('loggedin', function (username, city) {
+    console.log('[%s] Logged in %s as: %s',
+                (new Date()).toLocaleString(),
+				program.city || 'Berlin',
+                program.username);
+    game.collect(program.time);
+});
+
+game.on('start_collect', function (remaining_seconds) {
+    console.log('[%s] Collecting for %s Minutes', 
+				(new Date()).toLocaleString(), 
+				formatTime(remaining_seconds));
+    setTimeout(function () { game.clear_cart(); }, remaining_seconds * 1000);
+});
+
+game.on('pending_collect', function (remaining_seconds) {
+    console.log('[%s] Still collecting for %s Minutes', 
+				(new Date()).toLocaleString(), 
+				formatTime(remaining_seconds));
+    setTimeout(function () { game.clear_cart(); }, remaining_seconds * 1000);
+});
+
+game.on('clear_cart', function () {
+    game.collect(program.time);
+});
+
+game.on('pending_cart', function () {
+    game.clear_cart();
+});
